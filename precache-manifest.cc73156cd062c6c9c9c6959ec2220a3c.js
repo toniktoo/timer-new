dd4dcf3c30507df3b02c2d835a481d3119b49c5c
toniktoo <toniktoo@mail.ru>
2020-05-23 03:48:03 +0300

    Updates

---
+++ precache-manifest.cc73156cd062c6c9c9c6959ec2220a3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8078e519871fe6f919e980042fdff86c",
+    "url": "/timer-new/index.html"
+  },
+  {
+    "revision": "089b2e6b0143aeefecad",
+    "url": "/timer-new/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "42518039d30ae39e46dd",
+    "url": "/timer-new/static/css/main.2bd84c97.chunk.css"
+  },
+  {
+    "revision": "089b2e6b0143aeefecad",
+    "url": "/timer-new/static/js/2.ac2054fa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/timer-new/static/js/2.ac2054fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42518039d30ae39e46dd",
+    "url": "/timer-new/static/js/main.196c5d35.chunk.js"
+  },
+  {
+    "revision": "30f2943355a1e41902e5",
+    "url": "/timer-new/static/js/runtime-main.6afaecc4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/timer-new/precache-manifest.cc73156cd062c6c9c9c6959ec2220a3c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timer-new/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
